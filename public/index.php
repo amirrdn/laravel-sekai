2e5037583094d93ac360334ada15580b413c105d
My mac <mac@macs-MacBook-Pro.local>
2024-12-20 19:19:05 +0700

    vercel config

--- public/index.php
+++ public/index.php
@@ -13,5 +13,16 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Handle the incoming request through the kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+// Send the response to the browser
+$response->send();
+
+// Terminate the request (clean up after the response)
+$kernel->terminate($request, $response);
